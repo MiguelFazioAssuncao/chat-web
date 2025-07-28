dbfdc2e152c1d2bac3ea1d8fe23bb0165a75fadf
Miguel Fazio de Assunção <faziomiguel0@gmail.com>
2025-07-28 13:47:47 -0300

    feat: criar serviço de registro com criptografia de senha e emissão de JWT

---
+++ backend/chatweb/src/main/java/com/miguelfazio/chatweb/service/AuthService.java
@@ -0,0 +1,31 @@
+package com.miguelfazio.chatweb.service;
+
+import com.miguelfazio.chatweb.dto.RegisterRequest;
+import com.miguelfazio.chatweb.entity.User;
+import com.miguelfazio.chatweb.repository.UserRepository;
+import com.miguelfazio.chatweb.security.JwtService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class AuthService {
+
+    private final UserRepository userRepository;
+    private final BCryptPasswordEncoder passwordEncoder;
+    private final JwtService jwtService;
+
+    public String register(RegisterRequest request) {
+
+        var user = User.builder()
+                .username(request.username())
+                .email(request.email())
+                .password(passwordEncoder.encode(request.password()))
+                .build();
+
+        userRepository.save(user);
+
+        return jwtService.generateToken(user);
+    }
+}
