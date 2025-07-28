bfd5e522141281d192648e913fc17c45b9a5d72f
Miguel Fazio de Assunção <faziomiguel0@gmail.com>
2025-07-28 13:48:20 -0300

    feat: implementa registro de usuários com checagem de conflito e retorno de token JWT

---
+++ backend/chatweb/src/main/java/com/miguelfazio/chatweb/controller/AuthController.java
@@ -0,0 +1,31 @@
+package com.miguelfazio.chatweb.controller;
+
+import com.miguelfazio.chatweb.dto.RegisterRequest;
+import com.miguelfazio.chatweb.repository.UserRepository;
+import com.miguelfazio.chatweb.service.AuthService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/auth")
+@RequiredArgsConstructor
+public class AuthController {
+
+    private final AuthService authService;
+    private final UserRepository userRepository;
+
+    @PostMapping("/register")
+    public ResponseEntity<String> register(@RequestBody RegisterRequest request) {
+        if (userRepository.findByEmail(request.email()).isPresent() || userRepository.findByUsername(request.username()).isPresent()) {
+         return ResponseEntity.status(HttpStatus.CONFLICT).body("Email ou username já existentes");
+        }
+
+        var token = authService.register(request);
+        return ResponseEntity.ok(token);
+    }
+}
