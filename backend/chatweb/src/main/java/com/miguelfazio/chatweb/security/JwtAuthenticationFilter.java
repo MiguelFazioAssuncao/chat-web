20d7b5972c90ee3623749e487d95083dd2f735a3
Miguel Fazio de Assunção <faziomiguel0@gmail.com>
2025-08-01 14:56:48 -0300

    feat: implementar filtro de autenticação JWT

---
+++ backend/chatweb/src/main/java/com/miguelfazio/chatweb/security/JwtAuthenticationFilter.java
@@ -0,0 +1,68 @@
+package com.miguelfazio.chatweb.security;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+import java.util.UUID;
+
+@Component
+public class JwtAuthenticationFilter extends OncePerRequestFilter {
+
+    private final JwtService jwtService;
+    private final CustomUserDetailsService customUserDetailsService;
+
+    public JwtAuthenticationFilter(JwtService jwtService, CustomUserDetailsService customUserDetailsService) {
+        this.jwtService = jwtService;
+        this.customUserDetailsService = customUserDetailsService;
+    }
+
+    @Override
+    protected void doFilterInternal(
+            HttpServletRequest request,
+            HttpServletResponse response,
+            FilterChain filterChain
+    ) throws ServletException, IOException {
+        final String authHeader = request.getHeader("Authorization");
+        if (authHeader == null || !authHeader.startsWith("Bearer ")) {
+            filterChain.doFilter(request, response);
+            return;
+        }
+        System.out.println("Authorization header: " + authHeader);
+
+        final String jwt = authHeader.substring(7);
+        UUID userId;
+
+        try {
+            userId = jwtService.extractUserId(jwt);
+        } catch (Exception e) {
+            e.printStackTrace();
+            filterChain.doFilter(request, response);
+            return;
+        }
+
+        if (userId != null && SecurityContextHolder.getContext().getAuthentication() == null) {
+            var userDetails = customUserDetailsService.loadUserById(userId);
+
+            var authToken = new UsernamePasswordAuthenticationToken(
+                    userDetails,
+                    null,
+                    userDetails.getAuthorities()
+            );
+            authToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+            SecurityContextHolder.getContext().setAuthentication(authToken);
+
+                    SecurityContextHolder.getContext().getAuthentication();
+        }
+
+        filterChain.doFilter(request, response);
+    }
+}
