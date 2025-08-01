9aa2893308ec57ea38eafc65b44acce75f3746e0
Miguel Fazio de Assunção <faziomiguel0@gmail.com>
2025-08-01 20:37:55 -0300

    feat: adicionar método extractUsername para extrair email do token JWT

--- backend/chatweb/src/main/java/com/miguelfazio/chatweb/security/JwtService.java
+++ backend/chatweb/src/main/java/com/miguelfazio/chatweb/security/JwtService.java
@@ -1,18 +1,17 @@
 package com.miguelfazio.chatweb.security;
 
 import com.miguelfazio.chatweb.entity.User;
-import org.springframework.stereotype.Service;
-
-import io.jsonwebtoken.*;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.security.Keys;
+import jakarta.annotation.PostConstruct;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
 
 import java.security.Key;
 import java.time.Duration;
-import java.util.Date;
-
-import org.springframework.beans.factory.annotation.Value;
-import jakarta.annotation.PostConstruct;
 import java.util.Base64;
+import java.util.Date;
 import java.util.UUID;
 
 @Service
@@ -52,5 +51,12 @@ public class JwtService {
         return UUID.fromString(subject);
     }
 
-
+    public String extractUsername(String token) {
+        return Jwts.parserBuilder()
+                .setSigningKey(JWT_SECRET)
+                .build()
+                .parseClaimsJws(token)
+                .getBody()
+                .get("email", String.class);
+    }
 }
