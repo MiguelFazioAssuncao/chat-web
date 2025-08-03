08f8d97a99e68f38771c35c440982158082c732e
Miguel Fazio de Assunção <faziomiguel0@gmail.com>
2025-08-02 22:13:58 -0300

    config: configurando websocket

---
+++ backend/chatweb/src/main/java/com/miguelfazio/chatweb/config/WebSocketConfig.java
@@ -0,0 +1,30 @@
+package com.miguelfazio.chatweb.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
+import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
+import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+
+@Configuration
+@EnableWebSocketMessageBroker
+public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
+
+    @Value("${app.jwt.reset-secret}")
+    private String jwtSecret;
+
+    @Override
+    public void registerStompEndpoints(StompEndpointRegistry registry) {
+        registry.addEndpoint("/ws")
+                .setHandshakeHandler(new CustomHandshakeHandler(jwtSecret))
+                .setAllowedOriginPatterns("*")
+                .withSockJS();
+    }
+
+    @Override
+    public void configureMessageBroker(MessageBrokerRegistry registry) {
+        registry.setApplicationDestinationPrefixes("/app");
+        registry.enableSimpleBroker("/topic", "/queue");
+    }
+}
